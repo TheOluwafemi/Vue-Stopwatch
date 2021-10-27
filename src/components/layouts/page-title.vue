6c3f0521988586997abdaee437fb1ad2dd7d073b
theoluwafemi <oluwafemidamilola21@gmail.com>
2021-10-27 18:08:12 +0100

    - style the the page title component

--- src/components/layouts/page-title.vue
+++ src/components/layouts/page-title.vue
@@ -1,5 +1,19 @@
 <template>
-  <nav>
-    <h1>Stopwatch</h1>
+  <nav class="nav">
+    <h1 class="title">Stopwatch</h1>
   </nav>
 </template>
+
+<style lang="scss" scoped>
+  .nav {
+    background-color: #212121;
+    height: 30%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .title {
+      font-size: 40px;
+      text-transform: uppercase;
+    }
+  }
+</style>
