b39c35b1aa0caac2ca3f0256a10230c8620dfdac
theoluwafemi <oluwafemidamilola21@gmail.com>
2021-10-27 15:04:55 +0100

    added title component and timing indicator comp
    - (on the call)

---
+++ src/components/timing/timing-indicator.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+    <div>
+      <h2 class="time">{{ hour }} : {{ minute }} : {{ seconds }}</h2>
+
+      <button v-if="!timerStarted" class="btn" @click="startTimer">Start</button>
+      <button v-else class="btn">Stop</button>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        hour: 0,
+        minute: 0,
+        seconds: 0,
+        timerStarted: false,
+      };
+    },
+
+    methods: {
+      startTimer() {
+        this.timerStarted = true;
+        setInterval(() => {
+          
+          this.seconds++;
+        }, 1000);
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .time {
+    font-size: 70px;
+  }
+
+  .btn {
+    font-size: 40px;
+  }
+</style>
