2d96123babea1930111ae64e0bdc37d706352c4e
theoluwafemi <oluwafemidamilola21@gmail.com>
2021-10-27 18:07:51 +0100

    add global styles and simple css reset

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,34 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  #app {
+    font-family: 'Roboto', Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #fff;
+    box-sizing: border-box;
+    font-size: 16px;
+    background: #212121;
   }
-}
+
+  // reset to normalize browser styles
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
 </style>
