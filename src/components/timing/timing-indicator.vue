c6717409c4c56dce57796bcb2798dad71b79f8fb
theoluwafemi <oluwafemidamilola21@gmail.com>
2021-10-27 17:22:52 +0100

    added timer logic
    - watch the centiSeconds for changes
    - watch the seconds for changes
    -  watch the minutes for changes
    - watch the hours for changes
    - implement stopTimer method
    - clearInterval on destroy

--- src/components/timing/timing-indicator.vue
+++ src/components/timing/timing-indicator.vue
@@ -1,10 +1,13 @@
 <template>
   <section>
     <div>
-      <h2 class="time">{{ hour }} : {{ minute }} : {{ seconds }}</h2>
+      <h2 class="time">
+        {{ hours | padZero }} : {{ minutes | padZero }} : {{ seconds | padZero }} :
+        {{ hSeconds | padDoubleZero }}
+      </h2>
 
       <button v-if="!timerStarted" class="btn" @click="startTimer">Start</button>
-      <button v-else class="btn">Stop</button>
+      <button v-else class="btn" @click="stopTimer">Stop</button>
     </div>
   </section>
 </template>
@@ -13,21 +16,76 @@
   export default {
     data() {
       return {
-        hour: 0,
-        minute: 0,
+        hours: 0,
+        minutes: 0,
         seconds: 0,
+        hSeconds: 0, //hundreth of a second
         timerStarted: false,
+        secondsInterval: null,
       };
+    },
+
+    filters: {
+      padZero: function (value) {
+        if (value < 10) return `0${value}`;
+        return value;
+      },
+
+      padDoubleZero: function (value) {
+        if (value > 10 && value < 100) return `${value}0`;
+        if (value < 10) return `${value}00`;
+        return value;
+      },
+    },
+
+    watch: {
+      hSeconds(oldValue, newValue) {
+        if (newValue === 99) {
+          this.seconds++;
+          this.hSeconds = 0;
+        }
+      },
+
+      seconds(oldValue, newValue) {
+        if (newValue === 59) {
+          this.minutes++;
+          this.seconds = 0;
+        }
+      },
+
+      minutes(oldValue, newValue) {
+        if (newValue === 59) {
+          this.hours++;
+          this.minutes = 0;
+          this.seconds = 0;
+        }
+      },
     },
 
     methods: {
       startTimer() {
         this.timerStarted = true;
-        setInterval(() => {
-          
-          this.seconds++;
-        }, 1000);
+        this.secondsInterval = setInterval(() => {
+          this.hSeconds++;
+        }, 10);
       },
+
+      stopTimer() {
+        clearInterval(this.secondsInterval);
+        this.resetTimer();
+        this.timerStarted = false;
+      },
+
+      resetTimer() {
+        this.hSeconds = 0;
+        this.seconds = 0;
+        this.minutes = 0;
+        this.hours = 0;
+      },
+    },
+
+    beforeDestroy() {
+      clearInterval(this.secondsInterval);
     },
   };
 </script>
